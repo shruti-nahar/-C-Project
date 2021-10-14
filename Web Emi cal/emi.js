0af4a50c661bee88b5aed0ece343b182d9504b68
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-10-14 12:03:43 +0530

    EMI  Calculator

--- Web Emi cal/emi.js
+++ Web Emi cal/emi.js
@@ -1,12 +1,15 @@
 
-function fun() {
-        let pay1 = '';
-        let princ1 = document.getElementById('demo1').value;
-        let term1 = document.getElementById('demo2').value;
-        let intr1 = document.getElementById('demo3').value / 1200;
-        
-        let a= Math.round(princ1 * intr1 / (1 - (Math.pow(1 / (1 + intr1), term1))) * 100) / 100;
-        let b = Math.round((document.getElementById('pay1').value * document.getElementById('demo2').value) * 100) / 100;
-        let c= Math.round((document.getElementById('gt1').value * 1 - document.getElementById('demo1').value * 1) * 100) / 100;
-       document.getElementById('pay1').innerHTML=a;
-}
+          function emi() {
+              if (document.getElementById('loan1').value == null || document.getElementById('loan1').value.length == 0 || document.getElementById('months1').value == null || document.getElementById('months1').value.length == 0 || document.getElementById('rate1').value == null || document.getElementById('rate1').value.length == 0) {
+                  document.getElementById('pay1').value = 'Data Reqd.'
+              }
+              else {
+                  var pay1 = '';
+                  var princ1 = document.getElementById('loan1').value;
+                  var term1 = document.getElementById('months1').value;
+                  var intr1 = document.getElementById('rate1').value / 1200;
+                  document.getElementById('pay1').value = Math.round(princ1 * intr1 / (1 - (Math.pow(1 / (1 + intr1), term1))) * 100) / 100;
+                  document.getElementById('gt1').value = Math.round((document.getElementById('pay1').value * document.getElementById('months1').value) * 100) / 100;
+                  document.getElementById('tintt1').value = Math.round((document.getElementById('gt1').value * 1 - document.getElementById('loan1').value * 1) * 100) / 100;
+              }
+          }
